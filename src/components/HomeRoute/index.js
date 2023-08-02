baf66374b5a22995ace75c88e22f9d249fc6a84a
rakeshpavan <rakeshpavan4@gmail.com>
2023-08-02 13:07:11 +0000

    solution

---
+++ src/components/HomeRoute/index.js
@@ -0,0 +1,11 @@
+import HeaderRoute from '../HeaderRoute'
+import './index.css'
+
+const HomeRoute = () => (
+  <>
+    <HeaderRoute />
+    <h1>hello</h1>
+  </>
+)
+
+export default HomeRoute
---
+++ src/components/LoginRoute/index.js
@@ -0,0 +1,100 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import './index.css'
+
+class LoginRoute extends Component {
+  state = {
+    username: '',
+    password: '',
+    errorMsg: '',
+    showErrorMsg: false,
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onsubmitSuccess = jwtToken => {
+    const {history} = this.props
+
+    Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
+    history.replace('/')
+  }
+
+  onsubmitFailure = errorMsg => {
+    this.setState({showErrorMsg: true, errorMsg})
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const apiUrl = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onsubmitSuccess(data.jwt_token)
+    } else {
+      this.onsubmitFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    const {showErrorMsg, username, password, errorMsg} = this.state
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
+    return (
+      <div className="LoginRoute_Container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png "
+          alt="website logo"
+          className="website_logo"
+        />
+
+        <form className="input_Container" onClick={this.submitForm}>
+          <label htmlFor="username" className="label">
+            USERNAME
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            className="input"
+            onChange={this.onChangeUsername}
+          />
+          <label htmlFor="username" className="label">
+            PASSWORD
+          </label>
+          <input
+            type="password"
+            id="username"
+            value={password}
+            placeholder="Password"
+            className="input"
+            onChange={this.onChangePassword}
+          />
+          <button type="submit" className="LoginButton">
+            Login
+          </button>
+          {showErrorMsg && <p>*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginRoute
